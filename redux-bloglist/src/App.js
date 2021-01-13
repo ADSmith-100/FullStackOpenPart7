9681d562fc6d688ced7b3d8c31b8914ca4b313b8
Adam Smith <coloradoasmith@gmail.com>
2021-01-12 17:30:11 -0700

    blogs are stored in redux store and can see blogs from backend 7.10 complete and app stable

--- redux-bloglist/src/App.js
+++ redux-bloglist/src/App.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NewBlog from "./components/NewBlog";
 import Blogs from "./components/Blogs";
 import Notification from "./components/Notification";
+import { useDispatch } from "react-redux";
+import { initializeBlogs } from "./reducers/blogReducer";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(initializeBlogs());
+  }, [dispatch]);
+
   return (
     <div>
       <h1>Blog App REDUX</h1>
--- redux-bloglist/src/reducers/blogReducer.js
+++ redux-bloglist/src/reducers/blogReducer.js
@@ -1,26 +1,35 @@
-const initialState = [
-  {
-    id: 1,
-    title: "the app state is in redux store",
-    author: "Ben Dover",
-    url: "www.suckass.com",
-    likes: 69,
-  },
-  {
-    id: 2,
-    title: "felching",
-    author: "Stupid GUY",
-    url: "www.suckass.com",
-    likes: 2,
-  },
-];
+import blogService from "../services/blogs";
 
-const blogReducer = (state = initialState, action) => {
-  if (action.type === "NEW_BLOG") {
-    return [...state, action.data];
+// const initialState = [
+//   {
+//     id: 1,
+//     title: "the app state is in redux store",
+//     author: "Ben Dover",
+//     url: "www.suckass.com",
+//     likes: 69,
+//   },
+//   {
+//     id: 2,
+//     title: "felching",
+//     author: "Stupid GUY",
+//     url: "www.suckass.com",
+//     likes: 2,
+//   },
+// ];
+
+const blogReducer = (state = [], action) => {
+  console.log("ACTION:", action);
+
+  switch (action.type) {
+    case "NEW_BLOG": {
+      return [...state, action.data];
+    }
+    case "INIT_BLOGS": {
+      return action.data;
+    }
+    default:
+      return state;
   }
-
-  return state;
 };
 
 const generateId = () => Number((Math.random() * 1000000).toFixed(0));
@@ -35,6 +44,16 @@ export const createBlog = (title, author, url) => {
       url,
       votes: 0,
     },
+  };
+};
+
+export const initializeBlogs = () => {
+  return async (dispatch) => {
+    const blogs = await blogService.getAll();
+    dispatch({
+      type: "INIT_BLOGS",
+      data: blogs,
+    });
   };
 };
 
